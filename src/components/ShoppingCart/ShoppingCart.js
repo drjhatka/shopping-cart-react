a47d99cecf31c2d31dc580da4e2e01d1bd15e05a
drjhatka <bishawjit12@gmail.com>
2023-03-26 15:17:11 +0600

    layout finally done

--- src/components/ShoppingCart/ShoppingCart.js
+++ src/components/ShoppingCart/ShoppingCart.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ShoppingCart = () => {
     return (
-        <div className='col-3 my-3 text-primary fixed'>
+        <div className='col-3 my-3 text-primary sticky-top h-100'>
             <div className="card">
                 <h5>Products in your cart</h5>
             </div>
